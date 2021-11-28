70c0ba8279eb7d36b78e8d7e4c55498649bddfcf
johnnormand10 <87495757+johnnormand10@users.noreply.github.com>
2021-11-27 18:57:07 -0600

    Update 3-music-collection.js

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,31 +2,66 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-function addToCollection(title , artist , yearPublished){
+function addToCollection(title , artist , yearPublished, tracks){
     let album = {
         title: title,
         artist: artist,
-        yearPublished: yearPublished
+        yearPublished: yearPublished,
+        tracks: tracks
     }
     collection.push(album);
     return album;
 }
 
-console.log(addToCollection("Acid Rap" , "Chance the Rapper" , 2013));
-console.log(addToCollection("Coloring Book" , "Chance the Rapper" , 2016));
-console.log(addToCollection("Hollywood's Bleeding" , "Post Malone" , 2019));
-console.log(addToCollection("Beerbongs & Bentleys" , "Post Malone" , 2018));
-console.log(addToCollection("Change of Scenary" , "Quinn XCII" , 2015));
-console.log(addToCollection("The Story of Us" , "Quinn XCII" , 2017));
+console.log(addToCollection("Acid Rap" , "Chance the Rapper" , 2013 , []));
+console.log(addToCollection("Coloring Book" , "Chance the Rapper" , 2016 , []));
+console.log(addToCollection("Hollywood's Bleeding" , "Post Malone" , 2019 , []));
+
+let track1 = {
+    name: "Paranoid",
+    duration: "3:40"
+}
+let track2 = {
+    name: "Zack And Codeine",
+    duration: "3:24"
+}
+let track3 = {
+    name: "Better Now",
+    duration: "3:50"
+}
+console.log(addToCollection("Beerbongs & Bentleys" , "Post Malone" , 2018 , [track1 , track2 , track3]));
+
+let track4 = {
+    name: "Another Day in Paradise",
+    duration: "4:13"
+}
+console.log(addToCollection("Change of Scenary" , "Quinn XCII" , 2015 , [track4]));
+
+let track5 = {
+    name: "Straightjacket",
+    duration: "3:25"
+}
+console.log(addToCollection("The Story of Us" , "Quinn XCII" , 2017 , [track5]));
+
 console.log(collection);
+
+
+
 
 function showCollection(arr){
     console.log(arr.length);
 
-    arr.forEach(album => console.log(`${album.title} by ${album.artist}, published in ${album.yearPublished}`));
+    for(let i = 0; i < arr.length; i++){
+        console.log(`${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}`);
+
+        for(let t = 0; t < collection[i].tracks.length; t++){
+            console.log(`${t+1}:${collection[i].tracks[t].name}: ${collection[i].tracks[t].duration}`);
+        }
+    }
 }
 
 showCollection(collection);
+
 
 function findByArtist(artist){  
     let artistArray =[];
@@ -52,7 +87,12 @@ function search(searchCriteria){
 
         for( let i = 0; i < collection.length; i++){
             if(collection[i].artist === searchCriteria.artist && collection[i].yearPublished === searchCriteria.yearPublished){
-                searchArray.push(collection[i]);
+                for( let t = 0; t < collection[i].tracks.length; t++){
+                    if(collection[i].tracks[t].name === searchCriteria.trackName){
+                        searchArray.push(collection[i]);
+                    }
+                }
+                
             }
         }
         return searchArray;
@@ -61,7 +101,18 @@ function search(searchCriteria){
         return collection;
     }
 }
+let searchCriteriaXCII = {
+    artist: "Quinn XCII",
+    yearPublished: 2017,
+    trackName: "Straightjacket",
+}
+console.log(search(searchCriteriaXCII));
 
-console.log(search({artist: "Post Malone" , yearPublished: 2018}));
+let searchCriteriaQuinn = {
+    artist: "Quinn XCII",
+    yearPublished: 2017,
+    trackName: "Another Day in Paradise",
+}
+console.log(search(searchCriteriaQuinn));
+
 console.log(search({artist: "Johnny Cash" , yearPublished: 1969}));
-
